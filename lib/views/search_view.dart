5eaa93303788dfd6e735db4fd38985f257c0976f
Adham167 <adhamabdelsalam3040@gmail.com>
2025-05-21 02:15:14 +0300

    finish search view

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -9,6 +9,26 @@ class SearchView extends StatelessWidget {
       appBar: AppBar(
         title: Text("Search City", style: TextStyle(color: Colors.white)),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Center(
+          child: TextField(
+            maxLines: 3,
+            decoration: InputDecoration(
+              hintText: "Enter City Name",
+              label: Text("Search"),
+              suffixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.orange),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
