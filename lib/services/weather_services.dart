ccb9d189f069ce4b66fd5e1714202075abd487d3
Adham167 <adhamabdelsalam3040@gmail.com>
2025-05-22 02:06:37 +0300

    finish weather service class

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,1 +1,31 @@
-class WeatherServices {}
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app20/models/weather_model.dart';
+
+class WeatherServices {
+  final Dio dio;
+
+  WeatherServices(this.dio);
+  final String baseUrl = "http://api.weatherapi.com/v1";
+  final String apiKey = "b9a64f9dc843485d83005738252201";
+  Future<Object> getCurrenWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+        "$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1",
+      );
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage =
+          e.response?.data['error']['message'] ??
+          "Oops! there was an error please try later";
+      throw (errorMessage);
+    } catch (e) {
+      log(e.toString());
+      throw ("Oops! there was an error please try later");
+    }
+  }
+}
